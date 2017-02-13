25bcd1ac13a3db23772613dd6b06396e308556c2
fexolm <a.artem060@gmail.com>
2017-02-13 14:37:41 +0300

    Начал работу над StateManager

---
+++ GServer/GServer/State/GameObject.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GServer.State
+{
+    public class Vector3
+    {
+        public float X { get; set; }
+        public float Y { get; set; }
+        public float Z { get; set; }
+    }
+    public class Quanternion : Vector3 
+    {
+        public float W { get; set; }
+    }
+    public class GameObject
+    {
+        public bool Interacting { get; set; }
+
+    }
+}
