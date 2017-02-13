dadf92fadcb882b74151d34f51536f029bf8a990
fexolm <a.artem060@gmail.com>
2017-02-13 20:09:14 +0300

    HandshakeTest

--- GServer/Unit-Tests/MessageTest.cs
+++ GServer/Unit-Tests/MessageTest.cs
@@ -25,6 +25,18 @@ namespace Unit_Tests
             Assert.AreEqual(mesBeforeSer.Header.TypeId, Mess.Header.TypeId);
         }
         [Test]
+        public void HandshakeTest()
+        {
+            var msg = Message.Handshake;
+            var bytes = msg.Serialize();
+            var dmsg = Message.Deserialize(bytes);
+
+            Assert.AreEqual(dmsg.Header.Ordered, msg.Header.Ordered);
+            Assert.AreEqual(dmsg.Header.Reliable, msg.Header.Reliable);
+            Assert.AreEqual(dmsg.Header.Sequensed, msg.Header.Sequensed);
+            Assert.AreEqual(dmsg.Header.Type, msg.Header.Type);
+        }
+        [Test]
         public void MessageBuilderTest()
         {
             Token token = new Token("token");
