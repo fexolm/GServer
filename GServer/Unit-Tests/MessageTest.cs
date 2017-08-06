56af0a3326105ff0c6c3959064b656b7b8498e8f
MisterArslan <ArsBurs@gmail.com>
2017-08-06 21:17:24 +0300

    Added SerializationTest.

--- GServer/Unit-Tests/MessageTest.cs
+++ GServer/Unit-Tests/MessageTest.cs
@@ -18,58 +18,5 @@ namespace Unit_Tests
             Assert.AreEqual(dmsg.Header.Sequenced, msg.Header.Sequenced);
             Assert.AreEqual(dmsg.Header.Type, msg.Header.Type);
         }
-        [Test]
-        public void SerializationTest()
-        {
-            var token = Token.GenerateToken();
-            var ds = DataStorage.CreateForWrite();
-            ds.Push(13);
-            ds.Push("word");
-            ds.Push(true);
-            ds.Push("hello world");
-            ds.Push(13.221F);
-            ds.Push(14.32D);
-            var msg = new Message((short)MessageType.Ack, Mode.Reliable | Mode.Sequenced, ds);
-            msg.MessageId = 123;
-            msg.ConnectionToken = token;
-            var bytes = msg.Serialize();
-            var newMsg = Message.Deserialize(bytes);
-
-            Assert.AreEqual(msg.Header.Type, newMsg.Header.Type);
-            Assert.AreEqual(msg.Header.MessageId, newMsg.Header.MessageId);
-            Assert.AreEqual(msg.Header.Ordered, newMsg.Header.Ordered);
-            Assert.AreEqual(msg.Header.Reliable, newMsg.Header.Reliable);
-            Assert.AreEqual(msg.Header.Sequenced, newMsg.Header.Sequenced);
-            Assert.AreEqual(msg.Header.ConnectionToken, newMsg.Header.ConnectionToken);
-
-            var readDs = DataStorage.CreateForRead(msg.Body);
-
-            Assert.AreEqual(13, readDs.ReadInt32());
-            Assert.AreEqual("word", readDs.ReadString());
-            Assert.AreEqual(true, readDs.ReadBoolean());
-            Assert.AreEqual("hello world", readDs.ReadString());
-            Assert.AreEqual(13.221F, readDs.ReadFloat());
-            Assert.AreEqual(14.32D, readDs.ReadDouble());
-        }
-        [Test]
-        public void DataStorageTest()
-        {
-            DataStorage ds = DataStorage.CreateForWrite();
-            ds.Push(13);
-            ds.Push("word");
-            ds.Push(true);
-            ds.Push("hello world");
-            ds.Push(13.221F);
-            ds.Push(14.32D);
-
-            DataStorage readDs = DataStorage.CreateForRead(ds.Serialize());
-
-            Assert.AreEqual(13, readDs.ReadInt32());
-            Assert.AreEqual("word", readDs.ReadString());
-            Assert.AreEqual(true, readDs.ReadBoolean());
-            Assert.AreEqual("hello world", readDs.ReadString());
-            Assert.AreEqual(13.221F, readDs.ReadFloat());
-            Assert.AreEqual(14.32D, readDs.ReadDouble());
-        }
     }
 }
