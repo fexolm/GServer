08d3ca1e6b456f5f727194c250ed8dc56d437e4b
Alekseev Artem <a.artem060@gmail.com>
2017-10-02 13:59:43 +0300

    feature(HostExtensions.cs) add ping

---
+++ GServer/GServer/HostExtensions.cs
@@ -0,0 +1,13 @@
+﻿using GServer.Messages;
+
+namespace GServer
+{
+    public static class HostExtensions
+    {
+        public static void Ping(this Host host) {
+            foreach (var connection in host.GetConnections()) {
+                host.Send(new Message((short) MessageType.Ping, Mode.Reliable), connection);
+            }
+        }
+    }
+}
