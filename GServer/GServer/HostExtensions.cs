6f7d66496fbcc7aed62240835ab4e45606ccd824
Artem Alekseev <a.artem060@gmail.com>
2019-03-17 23:51:51 +0300

    Update HostExtensions.cs

--- GServer/GServer/HostExtensions.cs
+++ GServer/GServer/HostExtensions.cs
@@ -7,17 +7,10 @@ namespace GServer
     {
         public static void Ping(this Host host)
         {
-            try
+            foreach (var connection in host.GetConnections())
             {
-                foreach (var connection in host.GetConnections())
-                {
-                    if (connection.EndPoint == null) continue;
-                    host.Send(new Message((short)MessageType.Ping, Mode.Reliable), connection);
-                }
-            }
-            catch
-            {
-                System.Console.WriteLine("kek");
+                if (connection.EndPoint == null) continue;
+                host.Send(new Message((short)MessageType.Ping, Mode.Reliable), connection);
             }
         }
     }
